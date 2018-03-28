06ac02e5d8073076c22e2ede449e8767e73db286
faiface <faiface@ksp.sk>
2018-03-28 17:37:01 +0200

    expr: add type info

--- expr/expr.go
+++ expr/expr.go
@@ -1,30 +1,43 @@
 package expr
 
-import "github.com/faiface/funky/parse/parseinfo"
+import (
+	"go/types"
+
+	"github.com/faiface/funky/parse/parseinfo"
+)
 
 type Expr interface {
 	leftString() string
 	rightString() string
 	String() string
 
+	TypeInfo() types.Type
 	SourceInfo() *parseinfo.Source
 }
 
 type (
 	Var struct {
+		TI   types.Type
 		SI   *parseinfo.Source
 		Name string
 	}
 
 	Appl struct {
-		Left, Right Expr
+		TI    types.Type
+		Left  Expr
+		Right Expr
 	}
 
 	Abst struct {
+		TI    types.Type
 		Bound *Var
 		Body  Expr
 	}
 )
+
+func (v *Var) TypeInfo() types.Type  { return v.TI }
+func (a *Appl) TypeInfo() types.Type { return a.TI }
+func (a *Abst) TypeInfo() types.Type { return a.TI }
 
 func (v *Var) SourceInfo() *parseinfo.Source  { return v.SI }
 func (a *Appl) SourceInfo() *parseinfo.Source { return a.Left.SourceInfo() }
