9f31a32bfa98396738d4d860e9f3ccbf6f87184d
faiface <faiface@ksp.sk>
2018-03-29 12:22:24 +0200

    parse: update definitions parsing with new 'def' keyword

--- parse/definitions.go
+++ parse/definitions.go
@@ -10,81 +10,76 @@ func Definitions(tokens []Token) (map[string][]expr.Expr, error) {
 	defs := make(map[string][]expr.Expr)
 
 	for len(tokens) > 0 {
-		name := tokens[0].Value
-		if IsReserved(name) {
+		if len(tokens) < 4 {
+			return nil, &Error{
+				tokens[len(tokens)-1].SourceInfo,
+				"incomplete definition",
+			}
+		}
+		if tokens[0].Value != "def" {
 			return nil, &Error{
 				tokens[0].SourceInfo,
+				"expected: def",
+			}
+		}
+
+		name := tokens[1].Value
+		if IsReserved(name) {
+			return nil, &Error{
+				tokens[1].SourceInfo,
 				fmt.Sprintf("unexpected symbol: %s", tokens[0].Value),
 			}
 		}
-		if len(tokens) < 2 || tokens[1].Value != ":" {
+
+		if tokens[2].Value != ":" {
 			return nil, &Error{
-				tokens[0].SourceInfo,
-				"expected :",
+				tokens[2].SourceInfo,
+				"expected: :",
 			}
 		}
-		equals := findNextTopLevel("=", tokens)
-		if equals == -1 {
+		equals := 3
+		for equals < len(tokens) && tokens[equals].Value != "=" {
+			equals++
+		}
+		if equals == len(tokens) {
 			return nil, &Error{
 				tokens[len(tokens)-1].SourceInfo,
-				"expected =",
+				"expected: =",
 			}
 		}
-		typ, err := Type(tokens[2:equals])
+
+		t, err := Type(tokens[3:equals])
 		if err != nil {
 			return nil, err
 		}
-		if typ == nil {
+		if t == nil {
 			return nil, &Error{
-				tokens[1].SourceInfo,
-				"no type information after :",
+				tokens[2].SourceInfo,
+				"no type after :",
 			}
 		}
-		nextColon := findNextTopLevel(":", tokens[equals:])
-		nextDef := nextColon - 1 + equals
-		if nextColon == -1 {
-			nextDef = len(tokens)
+
+		nextDef := equals + 1
+		for nextDef < len(tokens) && tokens[nextDef].Value != "def" {
+			nextDef++
 		}
-		exp, err := Expression(tokens[equals+1 : nextDef])
+
+		e, err := Expression(tokens[equals+1 : nextDef])
 		if err != nil {
 			return nil, err
 		}
-		if exp == nil {
+		if e == nil {
 			return nil, &Error{
 				tokens[equals].SourceInfo,
 				"no expression after =",
 			}
 		}
-		exp = exp.WithTypeInfo(typ)
-		defs[name] = append(defs[name], exp)
+
+		e = e.WithTypeInfo(t)
+		defs[name] = append(defs[name], e)
+
 		tokens = tokens[nextDef:]
 	}
 
 	return defs, nil
-}
-
-func findNextTopLevel(symbol string, tokens []Token) int {
-	round := 0  // ()
-	square := 0 // []
-	curly := 0  // {}
-	for i := range tokens {
-		switch tokens[i].Value {
-		case "(":
-			round++
-		case ")":
-			round--
-		case "[":
-			square++
-		case "]":
-			square--
-		case "{":
-			curly++
-		case "}":
-			curly--
-		}
-		if tokens[i].Value == symbol && round == 0 && square == 0 && curly == 0 {
-			return i
-		}
-	}
-	return -1
 }
