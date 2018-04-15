49f780ac5522958405184942964ef46b05b02d04
faiface <faiface@localhost.localdomain>
2018-04-15 18:21:28 +0200

    typecheck: implement error messages (except for ambiguous match error)

--- types/typecheck/infer.go
+++ types/typecheck/infer.go
@@ -2,39 +2,70 @@ package typecheck
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/faiface/funky/expr"
 	"github.com/faiface/funky/parse/parseinfo"
 	"github.com/faiface/funky/types"
 )
 
-type NotBoundError struct {
-	SourceInfo *parseinfo.Source
-	Name       string
-}
+type (
+	NotBoundError struct {
+		SourceInfo *parseinfo.Source
+		Name       string
+	}
+
+	CannotApplyError struct {
+		LeftSourceInfo  *parseinfo.Source
+		RightSourceInfo *parseinfo.Source
+		Cases           []struct {
+			Left  types.Type
+			Right types.Type
+			Err   error
+		}
+	}
+
+	NoMatchError struct {
+		SourceInfo *parseinfo.Source
+		TypeInfo   types.Type
+		Results    []InferResult
+	}
+
+	AmbiguousMatchError struct {
+		SourceInfo *parseinfo.Source
+		TypeInfo   types.Type
+		Results    []InferResult
+	}
+)
 
 func (err *NotBoundError) Error() string {
 	return fmt.Sprintf("%v: variable not bound: %s", err.SourceInfo, err.Name)
 }
 
-type CannotApplyError struct {
-	Cases []struct {
-		Left  expr.Expr
-		Right expr.Expr
-		Err   error
-	}
-}
-
 func (err *CannotApplyError) Error() string {
-	return "cannot apply"
+	s := fmt.Sprintf("%v: cannot apply; in case function has type:", err.LeftSourceInfo)
+	for _, cas := range err.Cases {
+		s += "\n" + cas.Left.String()
+		if cas.Err != nil {
+			s += "\n" + indent(cas.Err.Error())
+			continue
+		}
+		s += "\n  " + fmt.Sprintf("%v: argument does not match: %v", err.RightSourceInfo, cas.Right)
+	}
+	return s
 }
 
-type NoMatchErr struct {
-	Results []InferResult
+func (err *NoMatchError) Error() string {
+	s := fmt.Sprintf("%v: expression type does not match required type: %v\n", err.SourceInfo, err.TypeInfo)
+	s += "admissible types are:"
+	for _, r := range err.Results {
+		s += fmt.Sprintf("\n  %v", r.Type)
+	}
+	return s
 }
 
-func (err *NoMatchErr) Error() string {
-	return "no match"
+func (err *AmbiguousMatchError) Error() string {
+	return "ambiguous match (TODO)"
 }
 
 type InferResult struct {
@@ -72,7 +103,12 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 			}
 		}
 		if len(filtered) == 0 {
-			err = &NoMatchErr{results}
+			err = &NoMatchError{e.SourceInfo(), e.TypeInfo(), results}
+			results = nil
+			return
+		}
+		if len(filtered) > 1 {
+			err = &AmbiguousMatchError{e.SourceInfo(), e.TypeInfo(), results}
 			results = nil
 			return
 		}
@@ -115,7 +151,10 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 			}
 		}
 		results = nil
-		cannotApplyErr := new(CannotApplyError)
+		cannotApplyErr := &CannotApplyError{
+			LeftSourceInfo:  e.Left.SourceInfo(),
+			RightSourceInfo: e.Right.SourceInfo(),
+		}
 		for _, r1 := range results1 {
 			results2, err := infer(
 				varIndex,
@@ -125,10 +164,10 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 			)
 			if err != nil {
 				cannotApplyErr.Cases = append(cannotApplyErr.Cases, struct {
-					Left  expr.Expr
-					Right expr.Expr
+					Left  types.Type
+					Right types.Type
 					Err   error
-				}{r1.Expr, nil, err})
+				}{r1.Type, nil, err})
 			}
 			resultType := newVar(varIndex)
 			for _, r2 := range results2 {
@@ -141,10 +180,10 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 				)
 				if !ok {
 					cannotApplyErr.Cases = append(cannotApplyErr.Cases, struct {
-						Left  expr.Expr
-						Right expr.Expr
+						Left  types.Type
+						Right types.Type
 						Err   error
-					}{r1.Expr, r2.Expr, nil})
+					}{r1.Type, r2.Type, nil})
 					continue
 				}
 				t := s.ApplyToType(resultType)
@@ -165,14 +204,20 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 		return results, nil
 
 	case *expr.Abst:
-		var bindType types.Type
+		var (
+			bindType types.Type
+			bodyType = e.Body.TypeInfo()
+		)
 		if f, ok := e.TypeInfo().(*types.Func); ok {
 			bindType = f.From
+			if bodyType == nil {
+				bodyType = f.To
+			}
 		} else {
 			bindType = newVar(varIndex)
 		}
 		newLocal := local.Assume(e.Bound.Name, bindType)
-		bodyResults, err := infer(varIndex, global, newLocal, e.Body)
+		bodyResults, err := infer(varIndex, global, newLocal, e.Body.WithTypeInfo(bodyType))
 		if err != nil {
 			return nil, err
 		}
@@ -197,6 +242,20 @@ func infer(varIndex *int, global Defs, local Vars, e expr.Expr) (results []Infer
 	}
 
 	panic("unreachable")
+}
+
+func indent(s string) string {
+	var b strings.Builder
+	if len(s) > 0 {
+		b.WriteString("  ")
+	}
+	for _, r := range s {
+		b.WriteRune(r)
+		if r == '\n' {
+			b.WriteString("  ")
+		}
+	}
+	return b.String()
 }
 
 func newVar(varIndex *int) *types.Var {
