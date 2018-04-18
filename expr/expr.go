eea67b5e8d08cd423367a17d8490e509d22266e5
faiface <faiface@localhost.localdomain>
2018-04-18 18:45:03 +0200

    expr: add Switch expression

--- expr/expr.go
+++ expr/expr.go
@@ -36,26 +36,46 @@ type (
 		Bound *Var
 		Body  Expr
 	}
+
+	Switch struct {
+		TI    types.Type
+		SI    *parseinfo.Source
+		Expr  Expr
+		Cases []struct {
+			Alt  *Var
+			Body Expr
+		}
+	}
 )
 
-func (v *Var) TypeInfo() types.Type  { return v.TI }
-func (a *Appl) TypeInfo() types.Type { return a.TI }
-func (a *Abst) TypeInfo() types.Type { return a.TI }
+func (v *Var) TypeInfo() types.Type    { return v.TI }
+func (a *Appl) TypeInfo() types.Type   { return a.TI }
+func (a *Abst) TypeInfo() types.Type   { return a.TI }
+func (s *Switch) TypeInfo() types.Type { return s.TI }
 
-func (v *Var) SetTypeInfo(t types.Type)  { v.TI = t }
-func (a *Appl) SetTypeInfo(t types.Type) { a.TI = t }
-func (a *Abst) SetTypeInfo(t types.Type) { a.TI = t }
+func (v *Var) WithTypeInfo(t types.Type) Expr    { return &Var{t, v.SI, v.Name} }
+func (a *Appl) WithTypeInfo(t types.Type) Expr   { return &Appl{t, a.Left, a.Right} }
+func (a *Abst) WithTypeInfo(t types.Type) Expr   { return &Abst{t, a.SI, a.Bound, a.Body} }
+func (s *Switch) WithTypeInfo(t types.Type) Expr { return &Switch{t, s.SI, s.Expr, s.Cases} }
 
-func (v *Var) WithTypeInfo(t types.Type) Expr  { return &Var{TI: t, SI: v.SI, Name: v.Name} }
-func (a *Appl) WithTypeInfo(t types.Type) Expr { return &Appl{TI: t, Left: a.Left, Right: a.Right} }
-func (a *Abst) WithTypeInfo(t types.Type) Expr { return &Abst{TI: t, Bound: a.Bound, Body: a.Body} }
-
-func (v *Var) SourceInfo() *parseinfo.Source  { return v.SI }
-func (a *Appl) SourceInfo() *parseinfo.Source { return a.Left.SourceInfo() }
-func (a *Abst) SourceInfo() *parseinfo.Source { return a.SI }
+func (v *Var) SourceInfo() *parseinfo.Source    { return v.SI }
+func (a *Appl) SourceInfo() *parseinfo.Source   { return a.Left.SourceInfo() }
+func (a *Abst) SourceInfo() *parseinfo.Source   { return a.SI }
+func (s *Switch) SourceInfo() *parseinfo.Source { return s.SI }
 
 func (v *Var) Map(f func(Expr) Expr) Expr  { return f(v) }
 func (a *Appl) Map(f func(Expr) Expr) Expr { return f(&Appl{a.TI, a.Left.Map(f), a.Right.Map(f)}) }
 func (a *Abst) Map(f func(Expr) Expr) Expr {
 	return f(&Abst{a.TI, a.SI, a.Bound.Map(f).(*Var), a.Body.Map(f)})
+}
+func (s *Switch) Map(f func(Expr) Expr) Expr {
+	newCases := make([]struct {
+		Alt  *Var
+		Body Expr
+	}, len(s.Cases))
+	for i := range newCases {
+		newCases[i].Alt = s.Cases[i].Alt.Map(f).(*Var)
+		newCases[i].Body = s.Cases[i].Body.Map(f)
+	}
+	return f(&Switch{s.TI, s.SI, s.Expr.Map(f), newCases})
 }
--- expr/string.go
+++ expr/string.go
@@ -2,20 +2,25 @@ package expr
 
 import "fmt"
 
-//TODO: print type info (?)
+func (v *Var) leftString() string    { return v.Name }
+func (a *Appl) leftString() string   { return a.String() }
+func (a *Abst) leftString() string   { return "(" + a.String() + ")" }
+func (s *Switch) leftString() string { return "(" + s.String() + ")" }
 
-func (v *Var) leftString() string { return v.Name }
-func (a *Appl) leftString() string {
+func (v *Var) rightString() string    { return v.Name }
+func (a *Appl) rightString() string   { return "(" + a.String() + "}" }
+func (a *Abst) rightString() string   { return "(" + a.String() + ")" }
+func (s *Switch) rightString() string { return s.String() }
+
+func (v *Var) String() string { return v.Name }
+func (a *Appl) String() string {
 	return fmt.Sprintf("%s %s", a.Left.leftString(), a.Right.rightString())
 }
-func (a *Abst) leftString() string { return fmt.Sprintf("(λ%v %v)", a.Bound, a.Body) }
-
-func (v *Var) rightString() string { return v.Name }
-func (a *Appl) rightString() string {
-	return fmt.Sprintf("(%s %s)", a.Left.leftString(), a.Right.rightString())
-}
-func (a *Abst) rightString() string { return fmt.Sprintf("(λ%v %v)", a.Bound, a.Body) }
-
-func (v *Var) String() string  { return v.Name }
-func (a *Appl) String() string { return a.leftString() }
 func (a *Abst) String() string { return fmt.Sprintf("λ%v %v", a.Bound, a.Body) }
+func (s *Switch) String() string {
+	str := fmt.Sprintf("switch %v", s.Expr.String())
+	for _, cas := range s.Cases {
+		str += fmt.Sprintf(" case %v %v", cas.Alt.String(), cas.Body.String())
+	}
+	return str
+}
