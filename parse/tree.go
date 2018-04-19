6bb3ac3c0dd2b980dfa233019e2c30116a187cc7
faiface <faiface@localhost.localdomain>
2018-04-19 22:04:58 +0200

    parse: treat _ as a letter

--- parse/tree.go
+++ parse/tree.go
@@ -305,7 +305,7 @@ func MultiTree(tokens []Token) (Tree, error) {
 
 func hasLetterOrDigit(s string) bool {
 	for _, r := range s {
-		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' {
 			return true
 		}
 	}
