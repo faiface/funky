8e8413119f0251089c486e0c6d3a4689d9cec591
faiface <faiface@localhost.localdomain>
2018-04-23 15:48:10 +0200

    interpreters: add funkycmd interpreter

---
+++ interpreters/funkycmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/faiface/funky"
+	"github.com/faiface/funky/runtime"
+)
+
+func main() {
+	program := funky.Run("main")
+	in, out := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+loop:
+	for {
+		switch program.Alternative() {
+		case 0: // done
+			break loop
+		case 1: // putc
+			_, err := out.WriteRune(program.Field(0).Char())
+			handleErr(err)
+			program = program.Field(1)
+		case 2: // getc
+			err := out.Flush()
+			handleErr(err)
+			r, _, err := in.ReadRune()
+			handleErr(err)
+			program = program.Field(0).Apply(runtime.MkChar(r))
+		}
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
